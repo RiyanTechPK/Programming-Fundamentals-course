a3ae5e8e68a12c97fd95f243e5ff2cbcb08e8ccb
Riyan Ahmad <riyandogar786@gmail.com>
2026-02-28 17:24:35 +0500

    13th problem solved

---
+++ 13th.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<" -----------------        \n"
+      "     o   ^__^ ";
+cout<<"  "<<endl;
+cout<<"      o  (oo)\\______ ";
+cout<<"   "<<endl;
+cout<<"         (__)\\     )\\/\\ \n"
+      "             ||---w |     \n"
+      "             ||     ||    \n";}
