3405ef87d4f86efb27356f97d70cd5843875dea9
Riyan Ahmad <riyandogar786@gmail.com>
2026-02-28 17:26:33 +0500

    15th problem solved

---
+++ 15th.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;;
+main(){
+cout<< "     .   .   .  \n"
+       "      .   .   . \n"
+       "       .   .   .\n"
+       "      .   .   . \n"
+       "     .   .   .  \n"
+       "      .   .   . \n"
+       "       .   .   .\n"
+       "************************\n"
+       " *                    *  \n"
+       "  *                  *    \n"
+       "    *              *       \n"
+       "      *          *         \n"
+       "       **********          \n"
+       " #WEICOME TO F()()|)Y APPLICATION# ";}
