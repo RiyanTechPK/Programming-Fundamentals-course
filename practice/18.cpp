6cce329c6a58d7169fd114f0de1ae26a06f2d23f
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-05 00:28:53 +0500

    prac

---
+++ practice/18.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+main()
+
+{
+    for (int p = 0; p < 1; p++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    for (int l = 0; l < 2; l++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    for (int m = 0; m < 3; m++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    for (int j = 0; j < 5; j++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+}
