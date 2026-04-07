f693ee1b74fd2bfda9b07028aaaaf4279f2d776e
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-07 15:32:26 +0500

    save

---
+++ LoopPractice/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{   char choice;
+    cout<<"enter your choice ";
+    cin>>choice;
+    
+    while(choice!='N' && choice!='n'){
+        cout<<"You are happy";
+        break;
+
+    }
+}
