1cfd8949c43eafe8119bb99410863a879a7802bc
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-07 16:46:23 +0500

    save

---
+++ LoopPractice/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+   int num; 
+   int sum=0;
+cout<<"enter number ";
+cin>>num;
+while (num>=0){
+  sum=sum+num;
+    cout<<"enter number ";
+    cin>>num;
+      
+}cout<<sum;
+}
