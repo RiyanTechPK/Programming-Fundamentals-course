d6b5cefb0cc7e2f2f5fd6aedf49ec8f9216620cf
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-17 21:54:56 +0500

    save

---
+++ arrayPractice/6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+int sum =0;
+float average =0;
+int num[5]={1,2,3,4,5};
+for(int i=0; i<5; i++){
+    sum=sum+num[i];
+}average =  sum/5;
+cout<<"sum is "<<sum<<endl;
+cout<<"average is "<<average;
+}
