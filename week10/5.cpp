0d587b28964a7d7fb2e3aca895afd75c013ee471
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-21 17:00:31 +0500

    lab

--- week10/5.cpp
+++ week10/5.cpp
@@ -2,5 +2,10 @@
 #include<cmath>
 using namespace std;
 main(){
-    cout<<"number "
+    int num1, num2;
+    cout<<"Enter number ";
+    cin>>num1;
+    cout<<"Enter power ";
+    cin>>num2;
+    cout<<"number becomes "<<pow(num1,num2);
 }
