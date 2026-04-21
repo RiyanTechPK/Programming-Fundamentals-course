766de0bd7231f22b01ab79f0e0f22f7675b7b946
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-21 16:45:11 +0500

    lab

---
+++ week10/3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"enter 1st number ";
+    cin>>num1;
+    cout<<"Enter 2nd number  ";
+    cin>>num2;
+    cout<< "Greater number "<<max(num1,num2);
+
+}
