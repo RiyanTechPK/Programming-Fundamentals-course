a5371789b6c98f0751f07bb0f631690e2bfd002e
Riyan Ahmad <riyandogar786@gmail.com>
2026-04-24 00:40:20 +0500

    save

---
+++ loopPractice2/2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+for(int i=0; i<5; i++)
+
+    
+    {
+        cout<<"*";
+         cout<<endl;
+         for(int j=0; j<=i; j++){
+ cout<<" ";
+         }
+        
+    }
+   
+
+}
