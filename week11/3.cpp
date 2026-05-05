be9e506b9e99fa14ead2c56eea0a09817eb9e949
Riyan Ahmad <riyandogar786@gmail.com>
2026-05-05 20:36:35 +0500

    prac

---
+++ week11/3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    cout<<"how many rows";
+    int rows; 
+    cin>>rows;
+    cout<<"how many columns ";
+    int cols;
+    cin>>cols;
+    int matrix[rows][cols];
+
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<cols; j++)
+        {
+            cout<<"enter number";
+            cin>>matrix[i][j];
+        }
+    }
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<cols; j++)
+        {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+    
